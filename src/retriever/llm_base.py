18d8f5425a7a34fc2d348023555af078a2a00ec9
Omer <BBLN@users.noreply.github.com>
2024-11-09 20:55:40 +0200

    peft adapter

--- src/retriever/llm_base.py
+++ src/retriever/llm_base.py
@@ -4,7 +4,8 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_together import ChatTogether
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-from transformers import BitsAndBytesConfig
+from transformers import BitsAndBytesConfig, AutoTokenizer, AutoModelForCausalLM, pipeline
+from peft import PeftConfig, PeftModel
 
 DEFAULT_TEMPERATURE = 0.95
 
@@ -26,18 +27,19 @@ def get_model_by_name(model_name: str, temperature: float = DEFAULT_TEMPERATURE)
             bnb_4bit_compute_dtype="float16",
             bnb_4bit_use_double_quant=True,
         )
-        llm = HuggingFacePipeline.from_model_id(
-            model_id="microsoft/Phi-3.5-mini-instruct",
-            task="text-generation",
-            pipeline_kwargs=dict(
-                max_new_tokens=512,
-                do_sample=False,
-                return_full_text=False,
-            ),
-            model_kwargs={
-                'quantization_config': quantization_config
-            },
+        model_id="microsoft/Phi-3.5-mini-instruct"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantization_config)
+        model = PeftModel.from_pretrained(model, "../NLP-project/lora-query-generator-microsoft-phi-3.5-mini-instruct_20241109_003912")
+        pipe = pipeline(
+            "text-generation", 
+            model=model, 
+            tokenizer=tokenizer,
+            max_new_tokens=512,
+            do_sample=False,
+            return_full_text=False,
         )
+        llm = HuggingFacePipeline(pipeline=pipe)
 
         return ChatHuggingFace(llm=llm)
     if "llama" in model_name.lower() or "phi" in model_name.lower() or "mistral" in model_name.lower():
